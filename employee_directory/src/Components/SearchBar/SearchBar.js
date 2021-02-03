8e0f2d2032c4f0109c80487ecdd60ec74f142941
johndolac <johndolacjr@gmail.com>
2021-02-02 21:41:17 -0700

    fixed bootstrap bug

---
+++ employee_directory/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./SearchBar.css"
+
+export default function SearchBar(props) {
+
+    return (
+
+        <section className="filter-container">
+            <form className="filter-form form-group ">
+
+                <div className="row">
+                    <h4 className="col-md-12">Filter By Name: </h4>
+                    <input onChange={props.handleSearch} type="text"></input>
+                </div>
+
+               
+               <div className="row">
+                    <div className="form-check col-lg-6">
+                       
+                        <input type="checkbox" className="form-check-input" id="male" onChange={props.handleMaleCheck} />
+                        <p className="form-check-label" >Male:</p>
+                    </div>
+                    
+
+                    <div className="form-check col-lg-6">
+                        
+                        <input type="checkbox" className="form-check-input" id="female" onChange={props.handleFemaleCheck} />
+                        <p className="form-check-label">Female:</p>
+                    </div>
+                    </div>
+
+
+
+            </form>
+        </section>
+
+
+    )
+}
