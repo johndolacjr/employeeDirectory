4189c73cd0ff85323db17f704aa2ee08add0b1a2
johndolac <johndolacjr@gmail.com>
2021-02-02 19:51:50 -0700

    updated app.js, index.js, and data.js

---
+++ src/employee_directory/src/API.js
@@ -0,0 +1,6 @@
+// import axios from "axios";
+
+// const API = {
+//     key: () => axios.get("https://randomuser.me/api/?results=100"),
+// };
+// export default API
--- src/employee_directory/src/App.js
+++ src/employee_directory/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//Super common - always destructure hooks you are going to use (easiest way)
+// import { useState, useEffect } from "react";
+// import API from "./API.js"
 
-function App() {
+import React from "react";
+import EmployeeContainer from "./Components/EmployeeContainer/EmployeeContainer";
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <EmployeeContainer/>
+    </>
   );
 }
 
 export default App;
+
+  // const [data, setData] = useState({
+  //   search: "",
+  //   people: [],
+  // });
+  // console.log (data);
+
+  // useEffect (() => {
+  //   API.key().then(response => setData(response.data))
+  // }, []);
+  
+  // delete everything in the return 
+  // write my own code to call other components 
+
+// Components 
+  // List of people
+  // search bar 
+// file for API calls - see activity 
+
+// value = what the user puts in 
+// name is used in the string in the searchform function 
+
+// App.js dont use a lot of coade 
+
+
